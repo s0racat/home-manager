741ef8c3caa70cc138792cf161985179b4754745
Takumi <125882337+s0racat@users.noreply.github.com>
2025-11-16 03:15:42 +0900

    Add libglvnd to GPU libraries environment

--- modules/targets/generic-linux/gpu/gpu-libs-env.nix
+++ modules/targets/generic-linux/gpu/gpu-libs-env.nix
@@ -6,6 +6,7 @@
   intel-media-driver,
   nvidia-vaapi-driver,
   linuxPackages,
+  libglvnd,
   nvidia_x11 ? linuxPackages.nvidia_x11,
   addNvidia ? false,
 }:
@@ -16,6 +17,7 @@ buildEnv {
     mesa
     libvdpau-va-gl
     intel-media-driver
+    libglvnd
   ]
   ++ lib.optionals addNvidia [
     nvidia_x11
