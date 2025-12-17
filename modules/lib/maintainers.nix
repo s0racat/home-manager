8eac9e813c396c604bad2e05cfb3a2f05f55aa2a
soracat <125882337+s0racat@users.noreply.github.com>
2025-12-17 17:56:03 +0900

    Revert "Update maintainers.nix"
    This reverts commit 513263f6acc065894554a49edb9d52705c757782.

--- modules/lib/maintainers.nix
+++ modules/lib/maintainers.nix
@@ -507,7 +507,12 @@
     github = "Rosuavio";
     githubId = 7164552;
   };
-
+  rrvsh = {
+    name = "Mohammad Rafiq";
+    email = "rafiq@rrv.sh";
+    github = "rrvsh";
+    githubId = 20300874;
+  };
   rszamszur = {
     name = "Radosław Szamszur";
     email = "radoslawszamszur@gmail.com";
